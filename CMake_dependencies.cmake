c03dfd5f9d9a295c77f424cffb50fba47b845f02
Michal Lurie <Michal.Lurie@gmail.com>
2026-02-05 15:55:09 +0100

    Fix: Try  forcing harfbuzz to be built from source on mingw

--- CMake_dependencies.cmake
+++ CMake_dependencies.cmake
@@ -11,13 +11,19 @@ include(cmake/CPM.cmake)
 
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/cmake/modules")
 
-CPMAddPackage(
-    NAME harfbuzz
-    URL https://github.com/harfbuzz/harfbuzz/releases/download/12.3.0/harfbuzz-12.3.0.tar.xz
-    URL_HASH SHA256=8660ebd3c27d9407fc8433b5d172bafba5f0317cb0bb4339f28e5370c93d42b7
-    EXCLUDE_FROM_ALL TRUE
-    OPTIONS "BUILD_SHARED_LIBS ON" "HB_BUILD_UTILS OFF"
-)
+if(WIN32 AND (CMAKE_BUILD_TYPE STREQUAL "Release"))
+    set(CPM_USE_LOCAL_PACKAGES OFF)
+    CPMAddPackage(
+        NAME harfbuzz
+        URL https://github.com/harfbuzz/harfbuzz/releases/download/12.3.0/harfbuzz-12.3.0.tar.xz
+        URL_HASH SHA256=8660ebd3c27d9407fc8433b5d172bafba5f0317cb0bb4339f28e5370c93d42b7
+        EXCLUDE_FROM_ALL TRUE
+        OPTIONS "BUILD_SHARED_LIBS ON" "HB_BUILD_UTILS OFF"
+    )
+    unset(CPM_USE_LOCAL_PACKAGES)
+else()
+endif()
+
 CPMAddPackage(
     URI "gh:InCom-0/otfcc-lib_cmake#master"
     OPTIONS "BUILD_SHARED_LIBS OFF"
